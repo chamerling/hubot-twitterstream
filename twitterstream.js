d93e62ed2974560fa259ba06ef9892537670e10c
Christophe Hamerling <chamerling@linagora.com>
2016-10-20 17:50:49 +0200

    Use twit module and clean code

--- twitterstream.js
+++ twitterstream.js
@@ -21,80 +21,68 @@
 // Author:
 //   Christophe Hamerling
 
-var consumer_key = process.env.TWITTER_CONSUMER_KEY
-var consumer_secret = process.env.TWITTER_CONSUMER_SECRET
-var access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY
-var access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET
-
-var twitter = require('ntwitter')
-  , _ = require('underscore');
+var Twit = require('twit');
 
 var auth = {
-  "consumer_key" : consumer_key,
-  "consumer_secret" : consumer_secret,
-  "access_token_key" : access_token_key,
-  "access_token_secret" : access_token_secret
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 }
 
-var twit = new twitter(auth);
-twit.verifyCredentials(function (err, data) {
-  if (err) {
-    throw new Error(err);
-  }
-})
-
-var streams = []
+var streams = {};
+var twit = new Twit(auth);
 
 module.exports = function(robot) {
 
   robot.respond(/twitterstream watch (.*)$/i, function(msg) {
-    var tag = msg.match[1]
-    twit.stream('statuses/filter', {'track': tag}, function(stream) {
-      streams.push({key : tag, fn : stream});
+    var tag = msg.match[1];
 
-      stream.on('data', function (data) {
-        msg.send('@' + data.user.screen_name + " (" + data.user.name + ") - " + data.text + '\n');
-      });
+    var stream = T.stream('statuses/filter', {track: tag});
+    streams[tag] = stream;
 
-      stream.on('destroy', function(data) {
-        msg.send('I do not watch ' + tag + ' anymore...')
-      })
+    stream.on('tweet', function(tweet) {
+      msg.send('@' + tweet.user.screen_name + " (" + tweet.user.name + ") - " + tweet.text + '\n');
     });
-    msg.send('I started watching ' + tag);
+
+    stream.on('destroy', function(data) {
+      msg.send('I do not watch #' + tag + ' anymore...')
+    });
+
+    msg.send('I started to watch #' + tag);
   });
 
   robot.respond(/twitterstream unwatch (.*)$/i, function(msg) {
     var tag = msg.match[1]
-    var stream = _.find(streams, function(s) {
-      return (s.key == tag);
-    });
-    if (stream != undefined) {
-      stream.fn.destroy();
-      streams = _.without(streams, _.findWhere(streams, stream));
-      msg.send('I stopped watching ' + tag);
-    } else {
-      msg.send('I do not known such tag.');
+    var stream = streams[tag];
+    if (!stream) {
+      return msg.send('I do not listen to tweets with tag #' + tag);
     }
+
+    stream.destroy();
+    delete streams[tag];
+    msg.send('I stopped to watch #' + tag);
   });
 
   robot.respond(/twitterstream list/i, function(msg) {
-    if (streams.length > 0) {
-      _.each(streams, function(s) {
-        msg.send(s.key);
-      });
-    } else {
-      msg.send('I have no tags.');
+
+    msg.send('I listen to tweets:');
+    for (var key in streams) {
+      if (streams.hasOwnProperty(key)) {
+        msg.send('- ' + key);
+      }
     }
+    msg.send('Hint: Type \'twitterstream watch XXX\' to listen to XXX tweets');
   });
 
   robot.respond(/twitterstream clear/i, function(msg) {
-    if (streams.length > 0) {
-      _.each(streams, function(s) {
-        s.fn.destroy();
-        streams = _.without(streams, _.findWhere(streams, s));
-      });
-    } else {
-      msg.send('I have no tags.');
+    for (var key in streams) {
+      if (streams.hasOwnProperty(key)) {
+        var stream = streams[key];
+        if (stream) {
+          stream.destroy();
+        }
+      }
     }
   });
-}
+};
